be9116042ee10c9f1bfc62ef6a61433f90ece5cc
SarahNk1-Maker <114023598+SarahNk1-Maker@users.noreply.github.com>
2023-09-01 00:09:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+
+$(document).ready(function () {
+    var dateTime = $('#dateTime');
+    var currentHour = moment().hour();
+    // Function to update the date and time
+    function updateDateTime() {
+      const now = new Date();
+      const currentHour = now.getHours();
+      const currentDateTime = now.toLocaleString();
+      document.querySelector('#datetime').textContent = currentDateTime;
+      updateTimeBlock(currentHour);
+    }
+  
+    // Function to update the time block classes (past, present, future)
+    function updateTimeBlock() {
+      $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+        console.log("currentHour " + currentHour);
+        console.log("timeBlock " + timeBlock);
+
+          if (timeBlock < currentHour) {
+              $(this).removeClass("present future").addClass("past");
+          } else if (timeBlock === currentHour) {
+              $(this).removeClass("past future").addClass("present");
+          } else {
+              $(this).removeClass("past present").addClass("future");
+          }
+      });
+    
+    }
+    // Call the `updateDateTime` function every second
+    setInterval(updateDateTime, 1000);
+  
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+    
+        localStorage.setItem(time, value);
+    })
+    
+    $("#hour-9AM .description").val(localStorage.getItem("hour-9AM"));
+    $("#hour-10AM .description").val(localStorage.getItem("hour-10AM"));
+    $("#hour-11AM .description").val(localStorage.getItem("hour-11AM"));
+    $("#hour-12AM .description").val(localStorage.getItem("hour-12AM"));
+    $("#hour-12AM .description").val(localStorage.getItem("hour-13PM"));
+    $("#hour-12AM .description").val(localStorage.getItem("hour-14PM"));
+    $("#hour-12AM .description").val(localStorage.getItem("hour-15PM"));
+    $("#hour-12AM .description").val(localStorage.getItem("hour-16PM"));
+    $("#hour-12AM .description").val(localStorage.getItem("hour-17PM"));
+    
+    printDateTime();
+    updateTimeBlock();
+    
+    
+  });
+  
